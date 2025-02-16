34611d968802989c8bce6f06f15d349b05f14c9d
NoaShields <noashields@rhs.school.nz>
2025-02-17 09:53:31 +1300

    01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+#01_variables
+#Use sensible, descriptive variable names
+#Variable names in Python are always lowercase
+#Constant names are UPPERCASE
+#underscores and numbers
+#(as long as the first character is not a number)
+
+v4r14b12 = "Weird Variable"
+CONSTANT = "Never Changing"
